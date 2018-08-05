0c613fdb18b250bc49098fbf139069f7242374b6
projectfollor <42124029+projectfollor@users.noreply.github.com>
2018-08-06 03:57:15 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,123 @@
+<?php if (!empty($_POST)): ?>
+   
+    
+    
+    
+    <?php
+    
+    function strToHex($string){
+    $hex = '';
+    for ($i=0; $i<strlen($string); $i++){
+        $ord = ord($string[$i]);
+        $hexCode = dechex($ord);
+        $hex .= substr('0'.$hexCode, -2);
+    }
+    return strToUpper($hex);
+}
+
+
+
+    $a = htmlspecialchars($_POST["imdb"]) . ":". htmlspecialchars($_POST["q"]) .":". htmlspecialchars($_POST["lang"]) .":". htmlspecialchars($_POST["hash"])  ; 
+   echo strToHex($a);
+   echo "<br>";
+   echo "Send As Input Data To : 0x566F0F622ddA9d97536F7f6e3D528eE701FBdeF4"
+   ?>
+    
+    <br>
+    
+<?php else: ?>
+
+    <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
+        IMDB: <input type="text" name="imdb"><br>
+       Quality :  <select name="q">
+  <option value="Low">Low</option>
+  <option value="720">720</option>
+  <option value="1080">1080</option>
+  <option value="2k">2k</option>
+  <option value="4k">4k</option>
+</select><br>
+
+
+
+   Language : 
+<select name="lang" data-placeholder="Choose a Language...">
+  <option value="AF">Afrikanns</option>
+  <option value="SQ">Albanian</option>
+  <option value="AR">Arabic</option>
+  <option value="HY">Armenian</option>
+  <option value="EU">Basque</option>
+  <option value="BN">Bengali</option>
+  <option value="BG">Bulgarian</option>
+  <option value="CA">Catalan</option>
+  <option value="KM">Cambodian</option>
+  <option value="ZH">Chinese (Mandarin)</option>
+  <option value="HR">Croation</option>
+  <option value="CS">Czech</option>
+  <option value="DA">Danish</option>
+  <option value="NL">Dutch</option>
+  <option value="EN">English</option>
+  <option value="ET">Estonian</option>
+  <option value="FJ">Fiji</option>
+  <option value="FI">Finnish</option>
+  <option value="FR">French</option>
+  <option value="KA">Georgian</option>
+  <option value="DE">German</option>
+  <option value="EL">Greek</option>
+  <option value="GU">Gujarati</option>
+  <option value="HE">Hebrew</option>
+  <option value="HI">Hindi</option>
+  <option value="HU">Hungarian</option>
+  <option value="IS">Icelandic</option>
+  <option value="ID">Indonesian</option>
+  <option value="GA">Irish</option>
+  <option value="IT">Italian</option>
+  <option value="JA">Japanese</option>
+  <option value="JW">Javanese</option>
+  <option value="KO">Korean</option>
+  <option value="LA">Latin</option>
+  <option value="LV">Latvian</option>
+  <option value="LT">Lithuanian</option>
+  <option value="MK">Macedonian</option>
+  <option value="MS">Malay</option>
+  <option value="ML">Malayalam</option>
+  <option value="MT">Maltese</option>
+  <option value="MI">Maori</option>
+  <option value="MR">Marathi</option>
+  <option value="MN">Mongolian</option>
+  <option value="NE">Nepali</option>
+  <option value="NO">Norwegian</option>
+  <option value="FA">Persian</option>
+  <option value="PL">Polish</option>
+  <option value="PT">Portuguese</option>
+  <option value="PA">Punjabi</option>
+  <option value="QU">Quechua</option>
+  <option value="RO">Romanian</option>
+  <option value="RU">Russian</option>
+  <option value="SM">Samoan</option>
+  <option value="SR">Serbian</option>
+  <option value="SK">Slovak</option>
+  <option value="SL">Slovenian</option>
+  <option value="ES">Spanish</option>
+  <option value="SW">Swahili</option>
+  <option value="SV">Swedish </option>
+  <option value="TA">Tamil</option>
+  <option value="TT">Tatar</option>
+  <option value="TE">Telugu</option>
+  <option value="TH">Thai</option>
+  <option value="BO">Tibetan</option>
+  <option value="TO">Tonga</option>
+  <option value="TR">Turkish</option>
+  <option value="UK">Ukranian</option>
+  <option value="UR">Urdu</option>
+  <option value="UZ">Uzbek</option>
+  <option value="VI">Vietnamese</option>
+  <option value="CY">Welsh</option>
+  <option value="XH">Xhosa</option>
+</select>
+
+
+        <br>
+        Hash : <input type="text" name="hash"><br>
+        <input type="submit">
+    </form>
+<?php endif; ?>
---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>idenze</title>
+  
+  
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css'>
+
+      <style>
+	  body{
+  padding:20px 20px;
+}
+
+.results tr[visible='false'],
+.no-result{
+  display:none;
+}
+
+.results tr[visible='true']{
+  display:table-row;
+}
+
+.counter{
+  padding:8px; 
+  color:#ccc;
+}
+
+
+
+
+</style>
+
+  
+</head>
+
+<body>
+<center><h2>Idenze Index</h2> <a href=add.php> Submit Movie Torrent</a></center>
+
+
+
+    
+    
+
+  <div class="form-group pull-right">
+    <input type="text" class="search form-control" placeholder="What you looking for?">
+</div>
+<span class="counter pull-right"></span>
+<table class="table table-hover table-bordered results">
+  <thead>
+    <tr>
+    <th class=col-md-5 col-xs-5>Name</th>
+    <th class=col-md-3 col-xs-3>Quality</th>
+    <th class=col-md-3 col-xs-3>Year</th>
+    <th class=col-md-3 col-xs-3>Language</th>
+    <th class=col-md-3 col-xs-3>Rating</th>
+    <th class=col-md-3 col-xs-3>IMDB Page</th>
+    <th class=col-md-3 col-xs-3>Watch Now</th>
+    <th class=col-md-3 col-xs-3>Donate Seeder</th>
+	  
+	  
+	  
+    </tr>
+    <tr class="warning no-result">
+      <td colspan="4"><i class="fa fa-warning"></i> No result</td>
+    </tr>
+  </thead>
+  <tbody>
+
+
+
+<?php
+$i = 0;
+$jsondata = file_get_contents("http://idenze.herokuapp.com/api.php");
+$obj = json_decode($jsondata);
+$size = sizeof($obj);
+
+
+
+while($i < $size) {
+   $id = $obj[$i][0];
+   $q = $obj[$i][1];
+   
+   
+   $id = preg_replace('/[^a-zA-Z0-9]/', '', $id);
+
+
+   $jsonx = file_get_contents("http://getimdb.herokuapp.com/get/?id=".$id);
+   $json = json_decode($jsonx, true);
+    
+    
+$link =  "https://instant.io/#".$obj[$i][3];
+$seeder = $obj[$i][4];
+$lang = $obj[$i][2];
+
+
+echo "  <tr>
+    <td>$json[title]</td>
+    <td>$q</td>
+    <td>$json[year]</td>
+    <td>$lang</td>
+    <td>$json[rating]</td>
+    <td><a target=_blank href=https://www.imdb.com/title/$id>IMDB</a></td>
+    <td><a target=_blank href=$link>Watch Now</a></td>
+    <td><a target=_blank href=https://etherscan.io/address/$seeder> Donate </a></td>
+  </tr>
+  ";
+  
+  
+$i++;
+}
+
+
+
+?>
+
+
+
+
+
+	
+  </tbody>
+</table>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+
+
+    <script >
+	
+	$(document).ready(function() {
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+    
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+    
+  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' item');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+		  });
+});
+</script>
+
+
+
+
+</body>
+
+</html>
