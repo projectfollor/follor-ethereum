51064f2a2b1437c2f5f9f9516a78e9344d82f6a5
projectfollor <42124029+projectfollor@users.noreply.github.com>
2018-08-23 14:41:27 +0530

    Create site.php

---
+++ site.php
@@ -0,0 +1,15 @@
+<?php
+if ($handle = opendir('.')) {
+
+    while (false !== ($entry = readdir($handle))) {
+
+        if ($entry != "." && $entry != "..") {
+
+            echo "$entry\n";
+        }
+    }
+
+    closedir($handle);
+}
+
+?>
